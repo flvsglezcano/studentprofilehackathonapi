54eb4edd7471ad1ce9561b216beaeda20d477cad
Istvan Kajos <ikajos@flvs.net>
2023-02-28 11:09:01 -0500

    pushing interfaces

---
+++ GraphQL AzureFunction1/Service/IAddressTableStorageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL_AzureFunction1.Types;
+
+namespace GraphQLSPP.Service
+{
+    public interface IAddressTableStorageService
+    { 
+        Task<List<AddressEntity>> GetAllAddressEntitiesAsync();
+        Task<AddressEntity> GetAddressEntityAsync(int id);
+        Task<AddressEntity> UpsertAddressEntityAsync(string id);
+        Task DeleteStudentEntityAsync(int id);
+    }
+}
---
+++ GraphQL AzureFunction1/Service/IPhoneTableStorageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL_AzureFunction1.Types;
+
+namespace GraphQLSPP.Service
+{
+    public interface IPhoneTableStorageService
+    {
+        Task<List<PhoneEntity>> GetAllPhoneEntitiesAsync();
+        Task<PhoneEntity> GetPhoneEntityAsync(int id);
+        Task<PhoneEntity> UpsertPhoneEntityAsync(string id);
+        Task DeletePhoneEntityAsync(int id);
+    }
+}
